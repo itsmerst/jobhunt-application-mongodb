2c987b849897f3e3520506e11ecd6c0a8c7c4d19
Rutik Tonpe <rutiktonpe@gmail.com>
2025-05-23 10:03:08 +0530

    first commit

---
+++ src/main/java/com/hcltech/jobhunt_mongodb/JobhuntMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.hcltech.jobhunt_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobhuntMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobhuntMongodbApplication.class, args);
+	}
+
+}
